510bf3124ef47a6efca1e929062d8ba841ba1403
galiciaabraham <galicia.abraham@hotmail.com>
2024-05-07 13:48:42 -0600

    added first controller to work on

---
+++ controller/switchController.js
@@ -0,0 +1,5 @@
+let switchController = {};
+
+switchController.getAll((req, res) =>{ 
+    
+})
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
 const router = express.Router();
+const switchController = require('../controller/switchController');
 
 router.get('/', );
