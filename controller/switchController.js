0ce2b2e0a3b53e738dfb38d3edd4856b2132a20c
galiciaabraham <galicia.abraham@hotmail.com>
2024-05-08 08:23:38 -0600

    Builded initial MVC arquitecture to test connection to MongoDB

--- controller/switchController.js
+++ controller/switchController.js
@@ -1,5 +1,15 @@
+const mongodb = require('../model/dbConnection');
+
 let switchController = {};
 
-switchController.getAll((req, res) =>{ 
-    
-})
+
+switchController.getAll = async (req, res) => {
+    const data = await mongodb.getDb().db.collection('switch_games').find();
+    data.toArray().then((games) =>{
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200);
+        res.send(games);
+    });
+};
+
+module.exports = switchController;
--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,4 @@ const express = require('express');
 const router = express.Router();
 const switchController = require('../controller/switchController');
 
-router.get('/', );
+router.get('/', switchController.getAll);
