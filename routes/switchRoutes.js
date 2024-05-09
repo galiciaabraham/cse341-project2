2de80417149840997dab1a9a1e44abc914014b00
galiciaabraham <galicia.abraham@hotmail.com>
2024-05-09 11:28:22 -0600

    included validation process

--- routes/switchRoutes.js
+++ routes/switchRoutes.js
@@ -2,14 +2,28 @@ const express = require('express');
 const router = express.Router();
 const switchController = require('../controller/switchController');
 const errorHandler = require('../middleware/errorHandler');
+const validator = require('../middleware/validator')
 
 router.get('/', errorHandler.CatchErrors(switchController.getAll));
-router.get('/find/:gameId', errorHandler.CatchErrors( switchController.getOne));
+router.get('/find/:gameId',
+validator.gameSearchValidator(),
+validator.validate, 
+errorHandler.CatchErrors( switchController.getOne));
 
-router.post('/new-game', errorHandler.CatchErrors(switchController.addGame));
+router.post('/new-game', 
+validator.gameValidator(),
+validator.validate,
+errorHandler.CatchErrors(switchController.addGame));
 
-router.put('/update-game/:gameId', errorHandler.CatchErrors(switchController.updateGame));
-router.delete('/delete-game/:gameId', errorHandler.CatchErrors(switchController.deleteGame));
+router.put('/update-game/:gameId', 
+validator.gameValidator(),
+validator.validate,
+errorHandler.CatchErrors(switchController.updateGame));
+
+router.delete('/delete-game/:gameId', 
+validator.gameSearchValidator(),
+validator.validate, 
+errorHandler.CatchErrors(switchController.deleteGame));
 
 
 
